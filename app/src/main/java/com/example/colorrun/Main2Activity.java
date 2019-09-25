ed0aa72ca5dee3d4e62500dd24173b4415bc3579
RomainLagrange <romain.lagrange64@gmail.com>
2019-09-25 16:49:00 +0200

    chrono qui marche + design activity 2

--- app/src/main/java/com/example/colorrun/Main2Activity.java
+++ app/src/main/java/com/example/colorrun/Main2Activity.java
@@ -2,7 +2,9 @@ package com.example.colorrun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void goActivity3(View view){
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
     }
 }
