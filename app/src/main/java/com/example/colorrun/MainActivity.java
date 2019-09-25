b270afe5f1ef1bdac915a58615fea17e181d288d
= <marliat@orange.fr>
2019-09-25 15:36:32 +0200

    ICONE+PAGE DE PRESENTATION+pointeur sur 2 eme activité

--- app/src/main/java/com/example/colorrun/MainActivity.java
+++ app/src/main/java/com/example/colorrun/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.colorrun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void activity(View view){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+
+
 }
